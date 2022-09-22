19dba6343154c298512bda6d0bb36e272f70a9cb
1zwy <81381576+1zwy@users.noreply.github.com>
2022-09-21 23:00:24 -0400

    Create yeah.js

---
+++ yeah.js
@@ -0,0 +1,15 @@
+// custom shit bot, go away... monkey
+setTimeout(function(){
+  win.document.getElementsByClassName("px-0 mb-3 shadow_md_35 border_md shift_up_sm mix_item")[0].click()
+	win.document.getElementsByClassName("px-0 mb-3 shadow_md_35 border_md shift_up_sm mix_item")[1].click()
+	win.document.getElementsByClassName("px-0 mb-3 shadow_md_35 border_md shift_up_sm mix_item")[2].click()
+	win.document.getElementsByClassName("px-0 mb-3 shadow_md_35 border_md shift_up_sm mix_item")[3].click()
+	win.document.getElementById("item_img_4").click()
+	win.document.getElementById("tag_button_upgrade").click()
+	win.document.getElementById("tag_button_downgrade").click()
+	win.document.getElementById("tag_button_any").click()
+},5000)
+
+setTimeout(function(){
+  win.close()
+},10000)
